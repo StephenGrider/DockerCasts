0e554c4e59cf62836a16eaab32dc36e076f34756
stephen grider <ste.grider@gmail.com>
2018-09-03 09:21:48 -0600

    finished complex project

--- complex/client/src/App.js
+++ complex/client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib Calculator version 2</h1>
+            <h1 className="App-title">Fib Calculator version KUBERNETES!</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
