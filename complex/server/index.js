9588b465262dc28e645cb85546b4b3c722f92ee2
stephen grider <ste.grider@gmail.com>
2019-07-10 10:14:41 -0600

    skaffold

--- complex/server/index.js
+++ complex/server/index.js
@@ -40,9 +40,9 @@ app.get('/', (req, res) => {
 });
 
 app.get('/values/all', async (req, res) => {
-  const values = await pgClient.query('SELECT * from values');
+  // const values = await pgClient.query('SELECT * from values');
 
-  res.send(values.rows);
+  res.send([1, 2, 3]);
 });
 
 app.get('/values/current', async (req, res) => {
