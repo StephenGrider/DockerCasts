49579298c06553db208dc5aea49cb35418ffd088
Mindaugas Miskinis <intermezzolt@gmail.com>
2021-01-13 16:02:28 +0200

    BALBALBAL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Hello World
+          Hello World BALBALBAL
         </p>
       </div>
     );
