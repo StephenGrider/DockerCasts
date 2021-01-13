4dac97c55f8b88eda7136a656507a99ef7d4f65f
Mindaugas Miskinis <intermezzolt@gmail.com>
2021-01-13 13:54:21 +0200

    Changed App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          I was changed on the Feature branch
+          Hello
         </p>
       </div>
     );
