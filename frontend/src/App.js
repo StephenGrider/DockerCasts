371b4048c60002468922e764089ca53e0eb73407
Mindaugas Miskinis <intermezzolt@gmail.com>
2021-01-13 15:38:03 +0200

    Hello World

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Hello
+          Hello World
         </p>
       </div>
     );
